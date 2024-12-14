a07e5bb1ac9780cb93edf9b7ecb99be45ec8f34c
Kennedy Owusu <kennediowusu@gmail.com>
2024-12-14 11:41:12 +0000

    Refactor routes to explicitly define CRUD endpoints for Todo resource

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\v1\TodoController;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('todos', TodoController::class);
+    Route::get('todos', [TodoController::class, 'index']);
+    Route::post('todos', [TodoController::class, 'store']);
+    Route::get('todos/{todo}', [TodoController::class, 'show']);
+    Route::put('todos/{todo}', [TodoController::class, 'update']);
+    Route::delete('todos/{todo}', [TodoController::class, 'destroy']);
 });
